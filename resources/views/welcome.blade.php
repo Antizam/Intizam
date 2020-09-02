af770a88641db5c49718d73f306bc701d988a071
Aziz-Mo <azoz.go1996@gmail.com>
2020-09-02 14:52:56 +0300

    testone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Awadh is annoyting person
                 </div>
 
                 <div class="links">
