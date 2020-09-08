454465b6b4b642f689c3830c59d59d6d428164cb
Awad ALbaron <albroon2012@gmail.com>
2020-09-08 14:37:49 +0300

    code style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,6 @@
         <br><br><br><br><br><br><br><br><br>
  <!-- Navbar content -->
  <nav class="navbar navbar-expand-sm navbar-custom fixed-top justify-content-end">
-        <nav class="navbar navbar-expand-sm navbar-light bg-custom">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
