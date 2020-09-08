06882210715f23a10de60dd0c5998f5c822af16c
Awad ALbaron <albroon2012@gmail.com>
2020-09-08 11:22:18 +0300

    add card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,6 @@
         .navbar-custom { 
     background-color: #0c8676; 
     } 
-
         .btn-link1 {
     font-size: 100%;
     color: #ffffff;
@@ -27,13 +26,17 @@
     
 </head>
 
-
 <!-- Body -->
-
 <body>
 
-   <!-- Navbar content -->
-   <nav class="navbar navbar-expand-sm navbar-custom fixed-top justify-content-end">
+<!-- Header -->
+<header>
+
+<p> this tag for card proplem fix</p>
+ <h1>Intizam</h1>
+
+ <!-- Navbar content -->
+ <nav class="navbar navbar-expand-sm navbar-custom fixed-top justify-content-end">
         <nav class="navbar navbar-expand-sm navbar-light bg-custom">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -53,36 +56,50 @@
                     @endif
          </nav>
     </nav>
+</header>
 
+<!-- Card -->
 
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<div class="container">
+<div class="row justify-content-md-center">
+        <div class = "row">
+        <div class="col-7">
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+         <h5 class="card-title">For Schools</h5>
+         <i class="material-icons pmd-list-icon align-middle">notifications</i>
+        <p class="card-text">Its helps in organizing the process of students leaving the school and avoiding the crowding.</p>
+      </div>
+    </div>
   </div>
 </div>
 
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class = "row-8">
+        <div class="col-7">
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+         <h5 class="card-title">For Student Drivers</h5>
+        <p class="card-text">its helps by reducing the time of waiting for students to comes out from school.</p>
+     </div>
+  </div>
+ </div>
+</div>
+        <div class = "row">
+        <div class="col-7">
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+         <h5 class="card-title">For Students</h5>
+        <p class="card-text">it help by organize them and avoid the crowds of students in the school main gates.</p>
+        </div>
+      </div>
   </div>
 </div>
 
-
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
+</div>
 </div>
 
 
-<!-- End Body -->
 </body>
-
-
-
 
 <!-- Footer -->
 
