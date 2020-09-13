8607910131a138be90b17e772cbc575a66dfc2a3
Awad ALbaron <albroon2012@gmail.com>
2020-09-13 15:24:23 +0300

    admin sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,59 +1,62 @@
 @extends('layouts.app')
 <style>
-/* The sidebar menu */
-.sidenav {
+
+.sidebar {
   height: 100%; /* Full-height: remove this if you want "auto" height */
-  width: 15%; /* Set the width of the sidebar */
+  width: 250px; /* Set the width of the sidebar */
   position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-  z-index: 1; /* Stay on top */
-  top: 50; /* Stay at the top */
+  z-index: 1;
+  top: 0;
   left: 0;
-  background-color: #0c8676; /* Black */
+  background-color: #0c8676;
   overflow-x: hidden; /* Disable horizontal scroll */
-  padding-top: 20px;
+  padding-top: 70px;
 }
 
+
 /* The navigation menu links */
-.sidenav a {
+.sidebar a {
   padding: 6px 8px 6px 16px;
   text-decoration: none;
-  font-size: 25px;
-  color: #ffffff;
+  font-size: 23px;
+  color: white;
   display: block;
 }
 
 /* When you mouse over the navigation links, change their color */
-.sidenav a:hover {
-  color: #f1f1f1;
+.sidebar a:hover {
+  color: white;
 }
 
-/* Style page content */
 .main {
-  margin-left: 160px; /* Same as the width of the sidebar */
+  margin-left: 250px; /* Same as the width of the sidenav */
   padding: 0px 10px;
 }
 
+.sidebar a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
 /* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
 @media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 10px;}
+  .sidebar {padding-top: 15px;}
+  .sidebar a {font-size: 18px;}
 }
 </style>
+
 @section('content')
-<div class="container">
-    <!-- Side navigation -->
-<div class="sidenav">
-  <a href="#">About</a>
-  <a href="#">Services</a>
-  <a href="#">Services</a>
-  <a href="#">Services</a>
-  <a href="#">Clients</a>
-  <a href="#">Contact</a>
+<body>
+
+<div class="sidebar">
+  <a  href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
 </div>
 
-<!-- Page content -->
 <div class="main">
-  ...
+  <p>This the screen size is 700px or less.</p>
 </div>
-</div>
+
+</body>
 @endsection
