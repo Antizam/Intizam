3808009500447d6d9fbf9ec3bd92deba93cddbbc
Awad ALbaron <albroon2012@gmail.com>
2020-09-14 13:59:16 +0300

    sidebar a font size

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,20 @@
 <style>
 .sidebar {
   height: 100%; /* Full-height: remove this if you want "auto" height */
-  width: 250px; /* Set the width of the sidebar */
+  width: 15%; /* Set the width of the sidebar */
   position: fixed; /* Fixed Sidebar (stay in place on scroll) */
   z-index: 1;
   top: 0;
   left: 0;
   background-color: #0c8676;
   overflow-x: hidden; /* Disable horizontal scroll */
-  padding-top: 70px;
+  padding-top: 50px;
 }
 /* The navigation menu links */
 .sidebar a {
   padding: 15px 15px 15px 16px;
   text-decoration: none;
-  font-size: 23px;
+  font-size: 16px;
   color: white;
   display: block;
 }
@@ -25,6 +25,7 @@
   color:white;
     text-decoration:none;
     cursor:pointer;
+
 }
 .main {
   margin-left: 250px; /* Same as the width of the sidenav */
@@ -35,7 +36,7 @@
   color: white;
 }
 /* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
-@media screen and (max-height: 450px) {
+@media screen and (max-height: 400px) {
   .sidebar {padding-top: 15px;}
   .sidebar a {font-size: 18px;}
 }
