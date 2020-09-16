567a062530af67b625dd2796cb919fd2ee188e1a
Awad ALbaron <albroon2012@gmail.com>
2020-09-16 14:06:26 +0300

    readonly input tage

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -16,6 +16,7 @@
     /* Disable horizontal scroll */
     padding-top: 50px;
   }
+
   /* The navigation menu links */
   .sidebar a {
     padding: 15px 15px 15px 16px;
@@ -24,32 +25,44 @@
     color: white;
     display: block;
   }
+
   /* When you mouse over the navigation links, change their color */
   .sidebar a:hover {
     color: white;
     text-decoration: none;
     cursor: pointer;
   }
+
   .bg-colour1 {
     background-color: #0c8676 !important;
   }
+
   .btn-link1 {
     font-size: 100%;
     color: #ffffff;
     text-decoration: none;
   }
+
   .sidebar a:hover:not(.active) {
     background-color: #555;
     color: white;
   }
+
   /* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
   @media screen and (max-height: 400px) {
     .sidebar {
       padding-top: 15px;
     }
+
     .sidebar a {
       font-size: 18px;
     }
+
+  }
+
+  .text-left1 {
+    padding-left: 40% !important;
+
   }
 </style>
 
@@ -59,7 +72,7 @@
   <div class="container">
     <div class="sidebar">
       <a href="#home"><i class="fas fa-home"></i> Home</a>
-      <a href="#profile"><i class="fas fa-user"></i> Profile</a>
+      <a href="{{ route('user.profile', Auth::user()->id) }}"><i class="fas fa-user"></i> Profile</a>
       <a href="#student"><i class="fas fa-users"></i> Students</a>
       <a href="#Leaving_schedule"><i class="far fa-clock"></i> Leaving Schedule</a>
       <a href="#Screening_table"><i class="fas fa-tv"></i> Screen Table</a>
@@ -71,7 +84,7 @@
         <div class="col-md-8">
           <div class="card w-100 bg-colour1">
             <div class="card-body">
-              <div class="text-center font-weight-bold">{{ __('Profile') }}</div>
+              <h5><div class="text-center font-weight-bold">{{ __('Profile') }}</div></h5>
               <form method="POST" action="{{ route('register') }}">
                 @csrf
 
@@ -79,7 +92,8 @@
                   <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                   <div class="col-md-6">
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user['name'] }}" required autocomplete="name" autofocus>
+                    <label for="name"></label>
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user['name'] }}" readonly required autocomplete="name" autofocus>
                   </div>
                 </div>
 
@@ -87,7 +101,7 @@
                   <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Mobile') }}</label>
 
                   <div class="col-md-6">
-                    <input id="phone" type="tel" name="phone" pattern="[0-9]{10}" class="form-control @error('number') is-invalid @enderror">
+                    <input id="phone" type="tel" name="phone" pattern="[0-9]{10}" class="form-control @error('number') is-invalid @enderror" readonly>
 
                     @error('number')
                     <span class="invalid-feedback" role="alert">
@@ -101,7 +115,7 @@
                   <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                   <div class="col-md-6">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user['email'] }}" required autocomplete="email">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user['email'] }}" readonly required autocomplete="email">
 
                     @error('email')
                     <span class="invalid-feedback" role="alert">
@@ -111,40 +125,55 @@
                   </div>
                 </div>
 
-                <div class="form-group">
-                  <label for="inputAddress">Address</label>
-                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                </div>
 
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" class="form-control" id="inputCity">
-                  </div>
+                <div class="form-group row">
+                  <label for="exampleFormControlSelect1" class="col-md-4 col-form-label text-md-right">{{ __('City') }}</label>
+                  <div class="col-md-6">
 
-                  <div class="form-group col-md-4">
-                    <label for="inputState">State</label>
-                    <select id="inputState" class="form-control">
-                      <option selected>Choose...</option>
-                      <option>...</option>
+                    <select  class="form-control" id="exampleFormControlSelect1" onfocus="this.defaultIndex=this.selectedIndex;" onchange="this.selectedIndex=this.defaultIndex;">
+                      <option value="1">Country1</option>
+                      <option value="2">Country2</option>
+                      <option value="3">Country3</option>
+                      <option value="4">Country4</option>
+                      <option value="5">Country5</option>
+                      <option value="6">Country6</option>
+                      <option value="7" selected="selected">Country7</option>
+                      <option value="8">Country8</option>
+                      <option value="9">Country9</option>
                     </select>
                   </div>
+                </div>
 
-                  <div class="form-group col-md-2">
-                    <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control" id="inputZip">
+
+                <div class="form-group row">
+                  <label for="exampleFormControlSelect1" class="col-md-4 col-form-label text-md-right">{{ __('Neighborhood') }}</label>
+                  <div class="col-md-6">
+                    <select class="form-control" id="exampleFormControlSelect1" readonly>
+                      <option>Neighborhood 1</option>
+                      <option>Neighborhood 2</option>
+                      <option>Neighborhood 3</option>
+                      <option>Neighborhood 4</option>
+                      <option>Neighborhood 5</option>
+                    </select>
                   </div>
                 </div>
 
-                <button type="submit" class="btn btn-primary mb-4">Edit Profile</button>
-                <button type="submit" class="btn btn-primary mb-4">Change Password</button>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-sm text-justify text-left1">
+                      <button type="submit" class="btn btn-primary mb-4" href="#">Edit Profile</button>
+                      <button type="submit" class="btn btn-primary mb-4" href="#">Change Password</button>
+                    </div>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
         </div>
       </div>
     </div>
-
+  </div>
+  </div>
 
 </body>
 @endsection
