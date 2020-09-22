3f857a96d75111e67ac8e897e99b82ec6b99b785
Albaron-Awad <albroon2012@gmail.com>
2020-09-22 14:41:56 +0300

    student view

---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,118 @@
+@extends('layouts.app')
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<style>
+  .sidebar {
+    height: 100%;
+    /* Full-height: remove this if you want "auto" height */
+    width: 15%;
+    /* Set the width of the sidebar */
+    position: fixed;
+    /* Fixed Sidebar (stay in place on scroll) */
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #0c8676;
+    overflow-x: hidden;
+    /* Disable horizontal scroll */
+    padding-top: 50px;
+  }
+
+  /* The navigation menu links */
+  .sidebar a {
+    padding: 15px 15px 15px 16px;
+    text-decoration: none;
+    font-size: 16px;
+    color: white;
+    display: block;
+  }
+
+  /* When you mouse over the navigation links, change their color */
+  .sidebar a:hover {
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .bg-colour1 {
+    background-color: #0c8676 !important;
+  }
+
+  .btn-link1 {
+    font-size: 100%;
+    color: #ffffff;
+    text-decoration: none;
+  }
+
+  .sidebar a:hover:not(.active) {
+    background-color: #555;
+    color: white;
+  }
+
+  /* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
+  @media screen and (max-height: 400px) {
+    .sidebar {
+      padding-top: 15px;
+    }
+
+    .sidebar a {
+      font-size: 18px;
+    }
+  }
+
+  .text-left1 {
+    padding-left: 35% !important;
+  }
+</style>
+
+@section('content')
+
+<body>
+  <div class="container">
+    <div class="sidebar">
+      <br>
+      <a href="{{ route('home')}}"><i class="fas fa-home"></i> Home</a>
+      <a href="{{ route('user.profile', Auth::user()->id) }}"><i class="fas fa-user"></i> Profile</a>
+      <a href="{{ route('student.index', Auth::user()->id) }}"><i class="fas fa-users"></i> Students</a>
+      <a href="#Leaving_schedule"><i class="far fa-clock"></i> Leaving Schedule</a>
+      <a href="#Screening_table"><i class="fas fa-tv"></i> Screen Table</a>
+      <a href="#Settings"><i class="fas fa-cog"></i> Settings</a>
+      <a href="#Technical Support"><i class="far fa-question-circle"></i> Technical Support</a>
+    </div>
+
+    <div class="row">
+<div class="col-sm-12">
+<h4><div class="text-center font-weight-bold">{{ __('Edit Profile') }}</div></h4>
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Email</td>
+          <td colspan = 2>Actions</td>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($users->students as $student)
+        <tr>
+            <td>{{$student->std_id}}</td>
+            <td>{{$student->std_name}}</td>
+            <td>{{$student->std_email}}</td>
+            <td>
+                <a href="{{ route('student.edit',$student->std_id)}}" class="btn btn-primary">Edit</a>
+            </td>
+            <td>
+                <form action="{{ route('student.destroy', $student->std_id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+</div>
+
+</body>
+@endsection
