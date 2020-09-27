a1ab97e7e16724da070548a09bc19067365d4092
Albaron-Awad <albroon2012@gmail.com>
2020-09-27 12:26:09 +0300

    delete&store

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 
-use App\User;
-use App\students;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\User;
+use App\students;
+use Hash;
+
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class StudentsController extends Controller
 {
@@ -15,14 +19,24 @@ class StudentsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($id,Request $request)
+    public function index($id, Request $request)
     {
-        
         $users = User::find($id);
-        return view('student.index', compact('users'))->paginate(10);
-        
+        $users = DB::table('students')->where('user_id', $users->id)->paginate(6);
 
-        /*$user = User::find($id);
+        return view('student.index', ['students' => $users])
+            ->with('i');
+
+        /*
+        $users = User::find($id);
+        $users->load(['students' => function ($q){
+        $q->orderBy('created_at', 'desc')->paginate(6);
+        }]);
+        return view('student.index', compact('users'))
+        ->with('i', ($request->input('page', 1) - 1) * 5);
+
+       
+       $user = User::find($id);
 
         foreach ($user->students as $student) {
             return view('index', compact('student'));
@@ -49,7 +63,8 @@ class StudentsController extends Controller
      */
     public function create()
     {
-        //
+
+        return view('student.create');
     }
 
     /**
@@ -58,9 +73,41 @@ class StudentsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store($id, Request $request)
     {
-        //
+        $rules = [
+            'std_id' => 'required|digits:10|unique:students,std_id',
+            'std_name' => 'required|string|max:255',
+            'std_email' => 'required|email|unique:students,std_email',
+            'std_password' => 'required|min:8',
+        ];
+        $warning = [
+            "std_id.required" => "The Student ID is required ",
+            "std_id.unique" => "This ID is already store in the school",
+            "std_id.digits" => "The ID should be only 10 digits ",
+
+            "std_name.required" => "The Student Name is required ",
+
+            "std_email.required" => "The Student Email is required ",
+            "std_email.unique" => "This email is already store in the school",
+            "std_email.email" => "This email must be a valid email address,check the syntax !",
+
+            "std_password.required" => "The Students password is required",
+            "std_password.min" => "The Students password should be at least 8 character"
+        ];
+        $validatedData = $request->validate($rules, $warning);
+
+        $new_Students = new Students();
+
+
+        $info = $request->all();
+        $new_Students->user_id = $id;
+        $info['std_password'] = Hash::make($info['std_password']);
+        $new_Students->fill($info);
+        $new_Students->save();
+
+        $request->session()->flash('success', 'Your profile has been updated');
+        return redirect()->route('student.index', $id);
     }
 
     /**
@@ -69,9 +116,9 @@ class StudentsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show( Request $request)
+    public function show(Request $request)
     {
-       //
+        //
     }
 
     /**
@@ -80,9 +127,10 @@ class StudentsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($std_id, Request $request)
     {
-        //
+        $student = Students::find($std_id);
+        return view("student.edit", compact("student"));
     }
 
     /**
@@ -103,8 +151,11 @@ class StudentsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($std_id, Request $request)
     {
-        //
+        students::find($std_id)->delete();
+
+        $request->session()->flash('success', ' a student deleted successfully');
+        return redirect()->back();
     }
 }
