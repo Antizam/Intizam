e34604418b822f33c741577e9e8553cd6964f619
Albaron-Awad <albroon2012@gmail.com>
2020-09-27 12:23:50 +0300

    set std_id as Primary keys& delete relation col

--- app/students.php
+++ app/students.php
@@ -6,20 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class students extends Model
 {
-    public function User()
-    {
-    	return $this->belongsTo(User::class);
-    }
-
+    protected $primaryKey = 'std_id';
+    
     protected $fillable = [
         'std_id',
         'std_password',
         'std_name',
         'std_email',
-        'std_relation',
-        'std_relation_name',
-        'std_relation_number',
         'user_id'
      
     ];
+    
+    public function User()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+   
 }
