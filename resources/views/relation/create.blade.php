6ca2a1881c1493b0d25ae662cf89c0882cda3b7d
Albaron-Awad <albroon2012@gmail.com>
2020-09-29 15:08:07 +0300

    relations views

---
+++ resources/views/relation/create.blade.php
@@ -0,0 +1,177 @@
+@extends('layouts.app')
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<style>
+  .sidebar {
+    height: 100%;
+    /* Full-height: remove this if you want "auto" height */
+    width: 15%;
+    /* Set the width of the sidebar */
+    position: fixed;
+    /* Fixed Sidebar (stay in place on scroll) */
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #0c8676;
+    overflow-x: hidden;
+    /* Disable horizontal scroll */
+    padding-top: 50px;
+  }
+
+  /* The navigation menu links */
+  .sidebar a {
+    padding: 15px 15px 15px 16px;
+    text-decoration: none;
+    font-size: 16px;
+    color: white;
+    display: block;
+  }
+
+  /* When you mouse over the navigation links, change their color */
+  .sidebar a:hover {
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .bg-colour1 {
+    background-color: #0c8676 !important;
+  }
+
+  .btn-link1 {
+    font-size: 100%;
+    color: #ffffff;
+    text-decoration: none;
+  }
+
+  .sidebar a:hover:not(.active) {
+    background-color: #555;
+    color: white;
+  }
+
+  /* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
+  @media screen and (max-height: 400px) {
+    .sidebar {
+      padding-top: 15px;
+    }
+
+    .sidebar a {
+      font-size: 18px;
+    }
+
+  }
+
+  .text-left1 {
+    padding-left: 40% !important;
+
+  }
+</style>
+
+@section('content')
+
+<body>
+  <div class="container">
+    <div class="sidebar">
+      <a href="{{ route('home')}}"><i class="fas fa-home"></i> Home</a>
+      <a href="{{ route('user.profile', Auth::user()->id) }}"><i class="fas fa-user"></i> Profile</a>
+      <a href="{{ route('student.index', Auth::user()->id) }}"><i class="fas fa-users"></i> Students</a>
+      <a href="#Leaving_schedule"><i class="far fa-clock"></i> Leaving Schedule</a>
+      <a href="#Screening_table"><i class="fas fa-tv"></i> Screen Table</a>
+      <a href="#Settings"><i class="fas fa-cog"></i> Settings</a>
+      <a href="#Technical Support"><i class="far fa-question-circle"></i> Technical Support</a>
+    </div>
+
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card w-100 bg-colour1">
+            <div class="card-body">
+
+              @if ($errors->any())
+              <div class="alert alert-danger">
+                <ul>
+                  @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                  @endforeach
+                </ul>
+              </div><br />
+              @endif
+              <h5>
+                <div class="text-center font-weight-bold">{{ __('create new Relation') }}</div>
+              </h5>
+              <form action="{{ route('relation.store', $student->std_id) }}" method="post">
+                @csrf
+
+
+                <div class="card-body">
+                  @if(session('success'))
+                  <div class="alert alert-success" role="alert">
+                    {{session('success')}}
+                    @endif
+                  </div>
+
+
+                  <div class="form-group row">
+                    <label for="relation" class="col-md-4 col-form-label text-md-right">{{ __('Relation') }}</label>
+
+                    <div class="col-md-6">
+
+                      <input type="text" class="form-control @error('relation') is-invalid @enderror" name="relation" value="{{ old("relation") }}" required autocomplete="relation" autofocus>
+
+                      @error('relation')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="form-group row">
+                    <label for="relation_name" class="col-md-4 col-form-label text-md-right">{{ __('Rrelation Name') }}</label>
+
+                    <div class="col-md-6">
+
+                      <input type="text" class="form-control @error('relation_name') is-invalid @enderror" name="relation_name" value="{{ old("relation_name") }}" required autocomplete="relation_name" autofocus>
+
+                      @error('relation_name')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="form-group row">
+                    <label for="relation_number" class="col-md-4 col-form-label text-md-right">{{ __('Relation Phone number') }}</label>
+
+                    <div class="col-md-6">
+
+                      <input type="tel" class="form-control @error('relation_number') is-invalid @enderror" name="relation_number" value="{{ old("relation_number") }}" required autocomplete="relation_number" autofocus>
+
+                      @error('relation_number')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                      @enderror
+                    </div>
+                  </div>
+
+
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-sm text-justify text-left1">
+                        <button type="submit" class="btn btn-primary mb-4" href="#">New Relation</button>
+                      </div>
+                    </div>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+</body>
+@endsection
---
+++ resources/views/relation/index.blade.php
@@ -0,0 +1,136 @@
+@extends('layouts.app')
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<style>
+  .sidebar {
+    height: 100%;
+    /* Full-height: remove this if you want "auto" height */
+    width: 15%;
+    /* Set the width of the sidebar */
+    position: fixed;
+    /* Fixed Sidebar (stay in place on scroll) */
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: #0c8676;
+    overflow-x: hidden;
+    /* Disable horizontal scroll */
+    padding-top: 50px;
+  }
+
+  /* The navigation menu links */
+  .sidebar a {
+    padding: 15px 15px 15px 16px;
+    text-decoration: none;
+    font-size: 16px;
+    color: white;
+    display: block;
+  }
+
+  /* When you mouse over the navigation links, change their color */
+  .sidebar a:hover {
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .bg-colour1 {
+    background-color: #0c8676 !important;
+  }
+
+  .btn-link1 {
+    font-size: 100%;
+    color: #ffffff;
+    text-decoration: none;
+  }
+
+  .sidebar a:hover:not(.active) {
+    background-color: #555;
+    color: white;
+  }
+
+  /* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
+  @media screen and (max-height: 400px) {
+    .sidebar {
+      padding-top: 15px;
+    }
+
+    .sidebar a {
+      font-size: 18px;
+    }
+  }
+
+  .text-left1 {
+    padding-left: 35% !important;
+  }
+</style>
+
+@section('content')
+
+<body>
+  <div class="container">
+    <div class="sidebar">
+      <br>
+      <a href="{{ route('home')}}"><i class="fas fa-home"></i> Home</a>
+      <a href="{{ route('user.profile', Auth::user()->id) }}"><i class="fas fa-user"></i> Profile</a>
+      <a href="{{ route('student.index', Auth::user()->id) }}"><i class="fas fa-users"></i> Students</a>
+      <a href="#Leaving_schedule"><i class="far fa-clock"></i> Leaving Schedule</a>
+      <a href="#Screening_table"><i class="fas fa-tv"></i> Screen Table</a>
+      <a href="#Settings"><i class="fas fa-cog"></i> Settings</a>
+      <a href="#Technical Support"><i class="far fa-question-circle"></i> Technical Support</a>
+    </div>
+
+  
+    
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+      <p>{{ $message }}</p>
+    </div>
+    @endif
+
+
+    <div class="row">
+      <table class="table table-bordered table-striped table-sm text-center">
+        <thead class="thead-dark">
+          <tr>
+            <th>No</th>
+            <th>The Relation</th>
+            <th>Relation Name</th>
+            <th>Relation Phone number</th>
+            <th width="280px">Action</th>
+          </tr>
+        </thead>
+        @foreach($relations as $relation)
+        <tbody>
+          <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{$relation->relation}}</td>
+            <td>{{$relation->relation_name}}</td>
+            <td>{{$relation->relation_number}}</td>
+            <td>
+              <form action="{{ route('relation.destroy',$relation->relation_id) }}" method="POST">
+
+
+                <a class="btn btn-info" href="{{ route('relation.show',$relation->relation_id) }}">Show</a>
+
+                <a class="btn btn-primary" href="{{ route('relation.edit',$relation->relation_id) }}">Edit</a>
+
+                @csrf
+                @method('DELETE')
+
+                <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this Student ?')">Delete</button>
+              </form>
+            </td>
+          </tr>
+
+          @endforeach
+        </tbody>
+      </table>
+      <div class="col-lg-12 margin-tb">
+        <div class="btn  mb1 black bg-white">
+          <a href="{{ route('student.index', Auth::user()->id) }}"><i class="fas fa-arrow-left"></i> Back</a>
+        </div>
+      </div>
+    </div>
+    {{ $relations->links() }}
+</body>
+@endsection
